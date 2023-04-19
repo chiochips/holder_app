b6773062c00f4ea87c02b22567a0e0f746163e74
chiochips <118652876+chiochips@users.noreply.github.com>
2023-04-19 14:54:47 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
+import TryIcon from "@mui/icons-material/Try";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon /> Keeper <TryIcon />
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
